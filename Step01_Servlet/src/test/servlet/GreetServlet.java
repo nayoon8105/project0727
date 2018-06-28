ab6bb50e8c8acdcf0365f5402cb7f6cd7005097d
oli999 <oli999@naver.com>
2018-06-28 11:15:56 +0900

    Step01_Servlet 수정됨

---
+++ Step01_Servlet/src/test/servlet/GreetServlet.java
@@ -0,0 +1,44 @@
+package test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GreetServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+		//응답 인코딩 설정
+		response.setCharacterEncoding("utf-8");
+		//응답 컨텐츠 설정
+		response.setContentType("text/html;charset=utf-8");
+		//클라이언트에게 응답할수 있는 객체의 참조값 얻어오기
+		PrintWriter pw=response.getWriter();
+		//html 형식으로 응답하기 
+		pw.println("<!doctype html>");
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<meta charset='utf-8'/>");
+		pw.println("<title>오늘의 인사</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<p>안녕 여러분들~~~</p>");
+		pw.println("</body>");
+		pw.println("</html>");
+		pw.close();		
+	}
+}
+
+
+
+
+
+
+
+
+
