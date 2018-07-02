d61f93a741e0a8821ba1417c2ce74f6b5deecd21
oli999 <oli999@naver.com>
2018-07-02 15:19:45 +0900

    Step04_JSP 프로젝트 수정됨

---
+++ Step04_JSP/src/test/member/dao/MemberDao.java
@@ -0,0 +1,178 @@
+package test.member.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.member.dto.MemberDto;
+import test.util.DBConnect;
+
+public class MemberDao {
+	private static MemberDao dao;
+	private MemberDao() {}
+	public static MemberDao getInstance() {
+		if(dao==null) {
+			dao=new MemberDao();
+		}
+		return dao;
+	}
+	//회원 한명의 정보를 수정하는 메소드
+	public boolean update(MemberDto dto) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		int flag=0;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="UPDATE member SET name=?,addr=?"
+					+ " WHERE num=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getAddr());
+			pstmt.setInt(3, dto.getNum());
+			flag=pstmt.executeUpdate();
+		}catch(SQLException se) {
+			se.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		if(flag>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	//회원 한명의 정보를 리턴하는 메소드
+	public MemberDto getData(int num) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		MemberDto dto=null;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="SELECT name, addr FROM member"
+					+ " WHERE num=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				//MemberDto 객체를 생성해서 회원 정보를 담는다. 
+				dto=new MemberDto();
+				dto.setNum(num);
+				dto.setName(rs.getString("name"));
+				dto.setAddr(rs.getString("addr"));
+			}
+		}catch(SQLException se) {
+			se.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		return dto;
+	}
+	
+	//회원 정보를 삭제하는 메소드
+	public boolean delete(int num) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		int flag=0;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="DELETE FROM member WHERE num=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			flag=pstmt.executeUpdate();
+		}catch(SQLException se) {
+			se.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		if(flag>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	//회원 정보를 저장하는 메소드
+	public boolean insert(MemberDto dto) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		int flag=0;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="INSERT INTO member (num,name,addr)"
+					+ " VALUES(MEMBER_SEQ.NEXTVAL, ?, ?)";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getAddr());
+			flag=pstmt.executeUpdate();
+		}catch(SQLException se) {
+			se.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		if(flag>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public List<MemberDto> getList(){
+		//필요한 객체를 담을 지역변수 만들기
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		//회원목록을 담을 객체 생성
+		List<MemberDto> list=new ArrayList<>();
+		try{
+			conn=new DBConnect().getConn();
+			String sql="SELECT num,name,addr FROM member "
+					+ "ORDER BY num ASC";
+			pstmt=conn.prepareStatement(sql);
+			//sql 문 수행하고 결과셋 받아오기
+			rs=pstmt.executeQuery();
+			while(rs.next()){
+				int num=rs.getInt("num");
+				String name=rs.getString("name");
+				String addr=rs.getString("addr");
+				//회원 한명의 정보를 MemberDto 객체에 담는다.
+				MemberDto dto=new MemberDto(num, name, addr);
+				//MemberDto 객체의 참조값을 ArrayList 에 저장
+				list.add(dto);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e){}
+		}
+		return list;
+	}
+}
+
+
+
+
+
+
+
