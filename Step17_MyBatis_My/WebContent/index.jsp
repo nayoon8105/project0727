8e9c5d4a8fd80f85905554ff8ecbe74c94b25487
nayoon <nayoon8105@naver.com>
2018-07-27 18:38:38 +0900

    commit1

--- Step17_MyBatis_My/WebContent/index.jsp
+++
@@ -1,8 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%
-	// 직접적인 응답은 하지 않고 Controller 를 타게 함
-	String cPath=request.getContextPath();
-
-	response.sendRedirect(cPath+"/home.do");
-%>
--- Step17_MyBatis_My/src/test/controller/Action.java
+++
@@ -1,9 +0,0 @@
-package test.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public abstract class Action {
-	// 추상 메소드(요청 처리를 하는 메소드)
-	public abstract ActionForward execute(HttpServletRequest request, HttpServletResponse response); 
-}
